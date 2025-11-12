074731c302b40fcf85a0b7fc6ebc006f3fee3b45
Matheus de Freita Silva <Matheus.freitas_silva@outlook.com>
2025-11-12 11:39:55 -0300

    Criado DbContext

---
+++ EquilibraMais/DbConfig/EquilibraMaisDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EquilibraMais.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquilibraMais.DbConfig;
+
+public class EquilibraMaisDbContext :DbContext
+{
+    public EquilibraMaisDbContext(DbContextOptions<EquilibraMaisDbContext> options) : base(options) { }
+    
+    public DbSet<Empresa> Empresas { get; set; }
+    public DbSet<Setor> Setores { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Funcionario_Info> FuncionarioInfos { get; set; }
+}
